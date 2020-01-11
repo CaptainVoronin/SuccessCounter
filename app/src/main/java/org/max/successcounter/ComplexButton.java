bdfbb9dbe903b30f55ea3f6ae06ddc0df3d1fecd
CaptainVoronin <CaptainVoronin@gmail.com>
2020-01-11 17:17:56 +0300

    Элемент управления для экрана "новое упражнение"

---
+++ app/src/main/java/org/max/successcounter/ComplexButton.java
@@ -0,0 +1,48 @@
+package org.max.successcounter;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ComplexButton
+{
+    private final int layoutID = R.layout.complex_button_layout;
+    TextView tvTitle;
+    TextView tvText;
+    private Activity ctx;
+    private LinearLayout layout;
+    private String title;
+    private String text;
+    private View.OnClickListener clickListener;
+
+    public ComplexButton(Activity ctx, String title, String text, View.OnClickListener clickListener)
+    {
+        this.ctx = ctx;
+        this.title = title;
+        this.text = text;
+        this.clickListener = clickListener;
+    }
+
+    public LinearLayout inflate()
+    {
+        layout = (LinearLayout) ctx.getLayoutInflater().inflate(layoutID, null, false);
+        tvTitle = layout.findViewById(R.id.tvTitle);
+        tvTitle.setText(title);
+        tvTitle.setOnClickListener(clickListener);
+
+        tvText = layout.findViewById(R.id.tvText);
+        tvText.setText(text);
+        tvText.setOnClickListener(clickListener);
+
+        return layout;
+    }
+
+    public void setEnabled(boolean enabled)
+    {
+        if (enabled)
+            tvTitle.setTextColor(ctx.getColor(android.R.color.black));
+        else
+            tvTitle.setTextColor(ctx.getColor(android.R.color.darker_gray));
+    }
+}
