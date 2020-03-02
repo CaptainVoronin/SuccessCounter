84988cf2f64f623216b4e0e519d013e586981ae8
CaptainVoronin <CaptainVoronin@gmail.com>
2020-03-03 00:31:26 +0300

    The version has changed

--- app/src/main/java/org/max/successcounter/Version.java
+++ app/src/main/java/org/max/successcounter/Version.java
@@ -4,5 +4,6 @@ public class Version
 {
     public static final int MAJOR=0;
     public static final int MINOR=2;
-    public static final int BUILD=19;
+    public static final int BUILD=235;
+    public static String BUILD_DATE="03 March 2020";
 }
