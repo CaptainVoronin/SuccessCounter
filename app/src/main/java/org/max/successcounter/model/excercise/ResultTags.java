396b2e1f778d3058b8c00bf13f8d0816942789c7
CaptainVoronin <CaptainVoronin@gmail.com>
2020-03-08 21:36:26 +0300

    Cross-table class links tags and results

--- app/src/main/java/org/max/successcounter/model/excercise/ResultTags.java
+++ app/src/main/java/org/max/successcounter/model/excercise/ResultTags.java
@@ -1,5 +1,20 @@
 package org.max.successcounter.model.excercise;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import lombok.Data;
+
+@Data
+@DatabaseTable( tableName = "result_tags" )
 public class ResultTags
 {
+    @DatabaseField(generatedId = true, allowGeneratedIdInsert = true)
+    Integer id;
+
+    @DatabaseField ( canBeNull = false, columnDefinition = "INTEGER NOT NULL REFERENCES result( id ) ON DELETE CASCADE")
+    Integer resultID;
+
+    @DatabaseField ( canBeNull = false, columnDefinition = "INTEGER NOT NULL REFERENCES tags( id ) ON DELETE CASCADE")
+    Integer tagID;
 }
