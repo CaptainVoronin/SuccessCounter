c8fb89d98b52cacdfc75921fc68add4ea6ba67dc
CaptainVoronin <CaptainVoronin@gmail.com>
2020-03-10 22:46:52 +0300

    Lomboked

--- app/src/main/java/org/max/successcounter/model/HistoryItem.java
+++ app/src/main/java/org/max/successcounter/model/HistoryItem.java
@@ -2,74 +2,38 @@ package org.max.successcounter.model;
 
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+
 import org.max.successcounter.model.excercise.Result;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @DatabaseTable( tableName = "history")
 public class HistoryItem
 {
+    @Getter
+    @Setter
     @DatabaseField( generatedId = true )
     private Integer id;
 
+    @Getter
+    @Setter
     @DatabaseField
     private Integer points;
 
+    @Getter
+    @Setter
     @DatabaseField
     private Float percent;
 
-    @DatabaseField( foreign = true, columnName = "parent_id" )
+    @Getter
+    @Setter
+    @DatabaseField(foreign = true, columnName = "parent_id", columnDefinition = "INTEGER NOT NULL REFERENCES result( id ) ON DELETE CASCADE")
     private Result parent;
 
-    @DatabaseField( canBeNull = false )
+    @Getter
+    @Setter
+    @DatabaseField(canBeNull = false, columnDefinition = "INTEGER NOT NULL REFERENCES template( id ) ON DELETE CASCADE")
     private Integer templateID;
-
-    public Integer getTemplateID()
-    {
-        return templateID;
-    }
-
-    public void setTemplateID(Integer templateID)
-    {
-        this.templateID = templateID;
-    }
-
-    public Result getParent()
-    {
-        return parent;
-    }
-
-    public void setParent(Result parent)
-    {
-        this.parent = parent;
-    }
-
-    public Integer getId()
-    {
-        return id;
-    }
-
-    public void setId(Integer id)
-    {
-        this.id = id;
-    }
-
-    public Integer getPoints()
-    {
-        return points;
-    }
-
-    public void setPoints(Integer points)
-    {
-        this.points = points;
-    }
-
-    public void setPercent(Float percent)
-    {
-        this.percent = percent;
-    }
-
-    public Float getPercent()
-    {
-        return percent;
-    }
 
 }
