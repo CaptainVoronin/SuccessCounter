e623c8bcff47b98518f152cce6aeae43bc2d25cf
CaptainVoronin <CaptainVoronin@gmail.com>
2020-03-11 02:47:27 +0300

    New classes were added

--- app/src/main/java/org/max/successcounter/DatabaseConfigUtil.java
+++ app/src/main/java/org/max/successcounter/DatabaseConfigUtil.java
@@ -3,7 +3,6 @@ package org.max.successcounter;
 import com.j256.ormlite.android.apptools.OrmLiteConfigUtil;
 
 import org.max.successcounter.model.HistoryItem;
-import org.max.successcounter.model.excercise.CompoundExcercise;
 import org.max.successcounter.model.excercise.OptionDescription;
 import org.max.successcounter.model.excercise.Result;
 import org.max.successcounter.model.excercise.ResultTags;
