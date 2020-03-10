3c164cede2c260369f054b878660f018ca5e9a24
CaptainVoronin <CaptainVoronin@gmail.com>
2020-03-10 21:58:21 +0300

    Fields orderNum and color were added

--- app/src/main/java/org/max/successcounter/model/excercise/OptionDescription.java
+++ app/src/main/java/org/max/successcounter/model/excercise/OptionDescription.java
@@ -3,8 +3,6 @@ package org.max.successcounter.model.excercise;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
-import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +12,15 @@ public class OptionDescription
     Boolean firstDefault;
 
     Boolean lastDefault;
+
+
+    /**
+     * The primary key
+     */
+    @Getter
+    @Setter
+    @DatabaseField(generatedId = true)
+    Integer id;
 
     public Boolean getFirstDefault()
     {
@@ -35,24 +42,43 @@ public class OptionDescription
         this.lastDefault = lastDefault;
     }
 
-    @Getter
-    @Setter
-    @DatabaseField( generatedId = true )
-    Integer id;
-
+    /**
+     * The points which this option brings to the player
+     */
     @Getter
     @Setter
     @DatabaseField
     Integer points;
-
+    /**
+     * Text description for UI
+     */
     @Getter
     @Setter
     @DatabaseField
     String description;
-
+    /**
+     * The option order number in the list
+     */
+    @Getter
+    @Setter
+    @DatabaseField
+    int orderNum;
+    /**
+     * The RGB code of the background color for the option button in the exercise activity
+     * The default value is #001C2B
+     */
+    @Getter
+    @Setter
+    @DatabaseField
+    int color;
     @Getter
     @Setter
     @DatabaseField( canBeNull = false, foreign = true, columnName = "parent_id",
         columnDefinition = "INTEGER REFERENCES template(id) ON DELETE CASCADE" )
-    Template    parent;
+    Template parent;
+
+    public OptionDescription()
+    {
+        color = 0x001C2B;
+    }
 }
