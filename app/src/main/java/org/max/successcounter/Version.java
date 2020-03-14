2ffa347899a0698c7e56769aea658e33112672ba
CaptainVoronin <CaptainVoronin@gmail.com>
2020-03-15 01:07:03 +0300

    New version

--- app/src/main/java/org/max/successcounter/Version.java
+++ app/src/main/java/org/max/successcounter/Version.java
@@ -4,7 +4,7 @@ public class Version
 {
     public static final int MAJOR=0;
     public static final int MINOR=2;
-    public static final int BUILD = 751;
+    public static final int BUILD = 768;
     public static String BUILD_DATE = "13 March 2020";
     public static String NAME = "Dobermann";
     public static String buildType = "debug";
