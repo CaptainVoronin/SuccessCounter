4a2405daedcab9b0ef41f38c2b0ae71547e19e3a
CaptainVoronin <CaptainVoronin@gmail.com>
2020-03-15 12:34:03 +0300

    Functions moved thither the base class

--- app/src/main/java/org/max/successcounter/model/excercise/RunToExercise.java
+++ app/src/main/java/org/max/successcounter/model/excercise/RunToExercise.java
@@ -1,68 +1,6 @@
 package org.max.successcounter.model.excercise;
 
-import lombok.Getter;
-import lombok.Setter;
-
 public class RunToExercise extends SimpleExercise
 {
-    boolean finished;
 
-    @Getter
-    @Setter
-    int limit;
-
-    public RunToExercise()
-    {
-        super();
-        finished = false;
-    }
-
-    public RunToExercise(int limit)
-    {
-        super();
-        setLimit(limit);
-        finished = false;
-    }
-
-    @Override
-    public IStep addNewShot(int points)
-    {
-        boolean isFinished = false;
-        // Prevent add points to a finished exercise
-        if (isFinished())
-            return null;
-
-        IStep step = super.addNewShot(points);
-
-        if (template.getSuccesLimited())
-        {
-            // This is the case
-            // when a player must pocket exact number of balls
-            // and may miss any number of shots
-            if (getTotalPoints() == getLimit())
-                // the limit of successful shots is reached
-                isFinished = true;
-        }
-        else
-        {
-            if( steps.size() == getLimit() )
-                isFinished = true;
-        }
-
-        if (isFinished && !finished)
-        {
-            finished = isFinished;
-            publishFinishEvent();
-        }
-
-        return step;
-    }
-
-    @Override
-    public IStep undo()
-    {
-        if (isFinished())
-            finished = false;
-        return super.undo();
-    }
 }
